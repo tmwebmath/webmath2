741688b8bc1f196ac9a922717911d29ac62cbba7
tmwebmath <tmwebmath@gmail.com>
2015-03-05 09:49:47 +0000

    ajout test qui différencie profs et students

--- permission/urls.py
+++ permission/urls.py
@@ -5,5 +5,6 @@ from permission.views import *
 urlpatterns = patterns('',
     url(r'^home/$', home, name="home"),
     url(r'^teachers/$', teachers, name="teachers"),
+    url(r'^teachers2/$', teachers, name="teachers2"),
     url(r'^students/$', students, name="students"),
 )
--- permission/views.py
+++ permission/views.py
@@ -1,18 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
-
-def is_teacher(user):
-    return user.groups.filter(name='teachers').exists()
-    
-def is_student(user):
-    return user.groups.filter(name='students').exists()
-    
-# def teacher_required(view):
-#     return user_passes_test(login_required(view), is_teacher)
+from common.auth_utils import *
 
 @login_required
 def home(request):
-    return render(request, 'permission/home.html')
+    return render(request, 'permission/home.html', locals())
 
 @login_required
 @user_passes_test(is_teacher)
@@ -23,3 +15,7 @@ def teachers(request):
 @user_passes_test(is_student)
 def students(request):
     return render(request, 'permission/students.html')
+
+@teacher_required
+def teachers2(request):
+    return render(request, 'permission/teachers.html')
